d6f61c6dabaefbac5edd4cc3db2a940b397980f0
David <david.baumann2002@gmail.com>
2019-10-09 15:04:23 +0200

    Filter für Buchstaben

--- src/uebung1/Main.java
+++ src/uebung1/Main.java
@@ -5,17 +5,64 @@
  */
 package uebung1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+class DemoThread extends Thread {
+  
+    
+  
+  }
 /**
  *
  * @author DavidLindlbauer
  */
-public class Main {
+public class Main{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private static List<String> liste = new ArrayList<>();
+    private static List<String> zahlen = new ArrayList<>();
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        CSVReader();
+        zahlen.forEach(s -> System.out.println(s));
+        System.out.println(zahlen.size());
     }
+    
+    public static void CSVReader() throws FileNotFoundException
+    {
+        Scanner s = new Scanner(new File("numbers.csv"));
+        s.useDelimiter("\n");
+        
+        while(s.hasNext())
+        {
+            String line = s.nextLine();
+            String[] teile = line.split(":");
+            liste.addAll(Arrays.asList(teile));
+        }
+        for(int i = 0;i < liste.size();i++)
+        {
+            boolean zahl = true;
+            for(int j = 0;j < liste.get(i).length();j++)
+            {
+                if(liste.get(i).charAt(j) < 48 || liste.get(i).charAt(j) > 57)
+                {
+                    zahl = false;
+                }
+            }
+            if(zahl == true)
+            {
+                zahlen.add(liste.get(i));
+            }
+        }
+        
+        
+    }
+
+
+    
+    
     
 }
